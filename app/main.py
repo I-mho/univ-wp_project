ff3926d033337d1e2d8ca69b6195b1f310c26bac
spjellO <wndnjs4698@naver.com>
2024-06-09 00:50:03 +0900

    page, account

--- app/main.py
+++ app/main.py
@@ -15,21 +15,32 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # 데이터베이스 모델 정의
 Base = declarative_base()
 
+# 사용자 데이터베이스 정의
 class User(Base):
     __tablename__ = "users"
 
+    # 사용자 id
     id = Column(String, primary_key=True, index=True)
+    # 사용자 이름
     name = Column(String)
+    # 사용자 비밀번호
     password = Column(String)
+    # 사용자 세션
     session_id = Column(String)
 
+# 게시글 데이터베이스 정의
 class Post(Base):
     __tablename__ = "posts"
 
+    # 게시글 id
     id = Column(Integer, primary_key=True, index=True)
+    # 게시글 제목
     title = Column(String, index=True)
+    # 게시글 내용
     content = Column(Text)
+    # 게시글 작성자
     author = Column(String)
+    # 게시글 작성 시각
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 # 데이터베이스 초기화
@@ -46,9 +57,10 @@ def get_db():
     finally:
         db.close()
 
-def check_session(session_id: str = Cookie(None)):
+def check_session(session_id: str = Cookie(None), db: Session = Depends(get_db)):
     if session_id:
-        return True
+        user = db.query(User).filter(User.session_id == session_id).first()
+        return user if user else False
     else:
         return False
 
@@ -90,3 +102,35 @@ def sign_in(id: str = Form(...), password: str = Form(...), db: Session = Depend
         return response
     else:
         return {"message": "로그인 실패!"}
+
+@app.get("/logout")
+def logout():
+    res = RedirectResponse(url='/')
+    res.delete_cookie("session_id")
+    return res
+
+@app.get("/mypage")
+def mypage(req: Request, session: User = Depends(check_session)):
+    if session:
+        return templates.TemplateResponse("mypage.html", {"request": req, "user": session})
+    else:
+        return RedirectResponse(url='/sign_in')
+
+@app.get("/myaccount")
+def myaccount_page(req: Request, db: Session = Depends(get_db), session_id: str = Cookie(None)):
+    db_user = db.query(User).filter(User.session_id == session_id).first()
+    if db_user:
+        return templates.TemplateResponse("myaccount.html", {"request": req, "user": db_user})
+    else:
+        return RedirectResponse(url="/sign_in", status_code=303)
+
+@app.post("/myaccount/")
+def update_account(name: str = Form(...), password: str = Form(...), db: Session = Depends(get_db), session_id: str = Cookie(None)):
+    db_user = db.query(User).filter(User.session_id == session_id).first()
+    if db_user:
+        db_user.name = name
+        db_user.password = password
+        db.commit()
+        return RedirectResponse(url="/mypage", status_code=303)
+    else:
+        return RedirectResponse(url="/sign_in", status_code=303)
